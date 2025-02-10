fdda81a22b65d60bea2506e69ce337aa0273aa1f
Kyle Agostinelli <dataDistributor@protonmail.com>
2025-02-10 09:03:45 -0600

    Functional prototype w/ GUI

---
+++ cpp/build/CMakeFiles/LongYieldGUI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LongYieldGUI.dir/src/main.cpp.o"
+  "CMakeFiles/LongYieldGUI.dir/src/main.cpp.o.d"
+  "LongYieldGUI"
+  "LongYieldGUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LongYieldGUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/build/CMakeFiles/LongYieldGUI.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LongYieldGUI.
+# This may be replaced when dependencies are built.
